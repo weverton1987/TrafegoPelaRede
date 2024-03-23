4e328a4425c128fce48a6bd5913f7df7409a416f
WEVERTON MAGALHAES DA SILVA <wevertonmagsilva1@gmail.com>
2024-03-23 12:36:42 -0300

    enviando arquivos

--- client.py
+++ client.py
@@ -1,18 +1,35 @@
+'''receber mensagem'''
+# import socket
+
+# #criando um cliente.
+# #O cliente não precisa se estabelecer em nenhuma porta ele so precisa se conectar com alguém.
+# HOST = 'localhost'#aki é o mesmo ip do seu servidor.
+# PORT = 8002
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.connect((HOST, PORT))#conectando com o servidor, tem q ser uma tupla dentro do connect.
+
+# #o cliente enviando uma msg para o servidor.
+# #use o encode para codificara msg, ou seja para transformar a msg em binário
+# sock.send(input('Digite uma mensagem ').encode())
+
+# #receber msg do servidor
+# confirmacao = sock.recv(1024)
+# if confirmacao == b'ola':
+#     print('mensagem recebida')
+
+'''receber arquivo'''
 import socket
 
-#criando um cliente.
-#O cliente não precisa se estabelecer em nenhuma porta ele so precisa se conectar com alguém.
-HOST = 'localhost'#aki é o mesmo ip do seu servidor.
+HOST = 'localhost'
 PORT = 8002
 
+arquivo = open('python.png', 'rb')#abrindo a imagem
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))#conectando com o servidor, tem q ser uma tupla dentro do connect.
-
-#o cliente enviando uma msg para o servidor.
-#use o encode para codificara msg, ou seja para transformar a msg em binário
-sock.send(input('Digite uma mensagem ').encode())
-
-#receber msg do servidor
+sock.connect((HOST, PORT))
+sock.send(input('Digite o nome do arquivo: ').encode())
+sock.send(arquivo.read())
 confirmacao = sock.recv(1024)
-if confirmacao == b'ola':
+if confirmacao == b'ok':
     print('mensagem recebida')
--- server.py
+++ server.py
@@ -1,23 +1,41 @@
+'''recebendo msg'''
+# import socket
+
+# HOST = 'localhost' #serve pra informar de onde é o servidor, no caso vai ser na minha maquina.
+# PORT = 8002 # porta 8000 é do django então 8002 pra diferenciar caso ja esteja usando django.
+
+# #primeiro temos que informar que estamos utilizando o protocolo TCP/IP
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# #agora preciso falar para esse socket onde ele está rodando, qual o ip, qual a porta etc.
+# sock.bind((HOST, PORT))#dentro do bind tem uma tupla. Aki estou falando que ele vai rodar dentro desse ip e nessa porta em específico.
+
+# #Agora preciso falar para o servidor de quem ele vai escutar, vai ter pessoas fazendo requisições para este servidor.
+# sock.listen(5)
+# while True:
+#     #com essa variavel o servidor ja está funcionando.
+#     novoSock, _ = sock.accept()
+    
+#     #Fazer o servidor receber uma msg em binário, lembra que tudo que o servidor recebe é em binário.
+#     #o decode serve decodificar as msgs que chegam do cliente
+#     mensagem = novoSock.recv(1024).decode()
+    
+#     print(mensagem)
+#     #enviando msg para o cliente, no momento q o cliente entra em contato, os dados do clente fica salvo neste servidor.
+#     novoSock.send(b'ola')
+
+'''recebendo arquivo'''
 import socket
 
-HOST = 'localhost' #serve pra informar de onde é o servidor, no caso vai ser na minha maquina.
-PORT = 8002 # porta 8000 é do django então 8002 pra diferenciar caso ja esteja usando django.
-
-#primeiro temos que informar que estamos utilizando o protocolo TCP/IP
+HOST = 'localhost'
+PORT = 8002 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#agora preciso falar para esse socket onde ele está rodando, qual o ip, qual a porta etc.
-sock.bind((HOST, PORT))#dentro do bind tem uma tupla. Aki estou falando que ele vai rodar dentro desse ip e nessa porta em específico.
-
-#Agora preciso falar para o servidor de quem ele vai escutar, vai ter pessoas fazendo requisições para este servidor.
+sock.bind((HOST, PORT))
 sock.listen(5)
 while True:
-    #com essa variavel o servidor ja está funcionando.
     novoSock, _ = sock.accept()
-    
-    #Fazer o servidor receber uma msg em binário, lembra que tudo que o servidor recebe é em binário.
-    #o decode serve decodificar as msgs que chegam do cliente
-    mensagem = novoSock.recv(1024).decode()
-    print(mensagem)
-    #enviando msg para o cliente, no momento q o cliente entra em contato, os dados do clente fica salvo neste servidor.
-    novoSock.send(b'ola')
+    nomeArquivo = novoSock.recv(1024).decode()
+    novoArquivo = novoSock.recv(10000000000)#o valor no parenteses é a qntd maxima de pixeis que tem a imagem recebida
+    with open(f'arquivo/{nomeArquivo}.png', 'wb') as arq:
+        arq.write(novoArquivo)
+    novoSock.send(b'ok')

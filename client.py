990c05dc03fae3bc02db4e610badfc22c103bbf9
WEVERTON MAGALHAES DA SILVA <wevertonmagsilva1@gmail.com>
2024-03-23 11:37:06 -0300

    introducao

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+#criando um cliente.
+#O cliente não precisa se estabelecer em nenhuma porta ele so precisa se conectar com alguém.
+HOST = 'localhost'#aki é o mesmo ip do seu servidor.
+PORT = 8002
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))#conectando com o servidor, tem q ser uma tupla dentro do connect.
+
+#o cliente enviando uma msg para o servidor.
+#use o encode para codificara msg, ou seja para transformar a msg em binário
+sock.send(input('Digite uma mensagem ').encode())
+
+#receber msg do servidor
+confirmacao = sock.recv(1024)
+if confirmacao == b'ola':
+    print('mensagem recebida')
